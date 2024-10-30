00032ae329448445d666a5b7a8b015360225b3f2
Rudzeniapol <ffgg94802@gmail.com>
2024-10-30 19:45:29 +0300

    add MergeTwoSortedLists Task

--- SomeLeetCode/Program.cs
+++ SomeLeetCode/Program.cs
@@ -2,11 +2,24 @@
 using System.Diagnostics;
 using System.Text;
 
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0,
+        ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class Program
 {
     static void Main()
     {
-        Console.WriteLine(IsValid("()"));
+
     }
 
     public static string LongestCommonPrefix(string[] strs)
@@ -88,60 +101,82 @@ class Program
         }   
         return result;
     }
-    
-    public static bool IsValid(string s) {
-            Stack<char> stack = new Stack<char>();
-            for (int i = 0; i < s.Length; i++)
+
+    public static bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        for (var i = 0; i < s.Length; i++)
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
             {
-                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
-                {
-                    stack.Push(s[i]);
-                }
-                else
-                {
-                    if (stack.Count == 0)
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        char popped = stack.Pop();
-                        switch (popped)
-                        {
-                            case '[':
-                                if (s[i] != ']')
-                                {
-                                    return false;
-                                }
-
-                                break;
-                            case '{':
-                                if (s[i] != '}')
-                                {
-                                    return false;
-                                }
-
-                                break;
-                            case '(':
-                                if (s[i] != ')')
-                                {
-                                    return false;
-                                }
-
-                                break;
-                        }
-                    }
-                }
-            }
-
-            if (stack.Count == 0)
-            {
-                return true;                
+                stack.Push(s[i]);
             }
             else
             {
-                return false;
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                var popped = stack.Pop();
+                switch (popped)
+                {
+                    case '[':
+                        if (s[i] != ']') return false;
+
+                        break;
+                    case '{':
+                        if (s[i] != '}') return false;
+
+                        break;
+                    case '(':
+                        if (s[i] != ')') return false;
+
+                        break;
+                }
             }
 
+        if (stack.Count == 0)
+            return true;
+        return false;
     }
+    public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode result = null;
+        ListNode head = new ListNode();
+        result = head;
+        while (list1 != null || list2 != null)
+        {
+            if (list1 == null)
+            {
+                head.next = new ListNode(list2.val, null);
+                head = head.next;
+                list2 = list2.next;
+                continue;
+            }
+
+            if (list2 == null)
+            {
+                head.next = new ListNode(list1.val, null);
+                head = head.next;
+                list1 = list1.next;
+                continue;
+            }
+
+            if (list1.val < list2.val)
+            {
+                head.next = new ListNode(list1.val, null);
+                head = head.next;
+                list1 = list1.next;
+            }
+            else
+            {
+                head.next = new ListNode(list2.val, null);
+                head = head.next;
+                list2 = list2.next;
+            }
+        }
+        result = result.next;
+        return result;
+    }
+    
 }
